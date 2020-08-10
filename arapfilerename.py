872bd6d4fc76168a855c2e0e33781a3f1e3e4569
Ponson PC Sun <Ponson.PC.Sun@BenQ.com>
2020-08-10 11:44:06 +0800

    Version 1.0 Remove some debug information to make the output clear.

--- arapfilerename.py
+++ arapfilerename.py
@@ -20,35 +20,32 @@ print ('directory: %s' %fileList)
 for fileStr in fileList:
 	tarCountryIdx = -1
 	tarDataTypeIdx = -1
-	strLen = len(fileStr)
-	
-	print ("filename is %s" %fileStr)
-	print ("file length is %d" %strLen)
-	
+		
 	if fileStr.find(".csv") < 0:
 		continue
-
+	print ("filename : %s" %fileStr)
+	strLen = len(fileStr)
     # Data Type Comparison
 	for i in range(len(strDataTypesBefore)):
-		print(strDataTypesBefore[i])
-		print(fileStr.find(strDataTypesBefore[i]))
+		#print(strDataTypesBefore[i])
+		#print(fileStr.find(strDataTypesBefore[i]))
 		if fileStr.find(strDataTypesBefore[i]) >= 0:
 			tarDataTypeIdx = i
 			break
 			
 	for j in range(len(strCountriesBefore)):
-		print(strCountriesBefore[j])
-		print(fileStr[strLen-12: strLen-10])
+		#print(strCountriesBefore[j])
+		#print(fileStr[strLen-12: strLen-10])
 		if fileStr.find(strCountriesBefore[j], strLen-12, strLen-10) >= 0:
 			tarCountryIdx = j
 			break
 	idxStrP = "DataTypeIdx: {}, CountryIdx: {}" 		
-	print (idxStrP.format(tarDataTypeIdx, tarCountryIdx))
+	#print (idxStrP.format(tarDataTypeIdx, tarCountryIdx))
 	if (tarDataTypeIdx >= 0 and tarCountryIdx >=0):
 		#handle date
 		dateStr = fileStr[strLen-9:strLen-4]
 		dateObj = datetime.strptime("2020-"+dateStr, "%Y-%m-%d").date()
-		print(strCountriesAfter[tarCountryIdx]+strDataTypesAfter[tarDataTypeIdx]+str(dateObj)+"_to_"+str(dateObj)+".csv")
+		print("==> "+strCountriesAfter[tarCountryIdx]+strDataTypesAfter[tarDataTypeIdx]+str(dateObj)+"_to_"+str(dateObj)+".csv")
 		os.rename(fileStr,strCountriesAfter[tarCountryIdx]+strDataTypesAfter[tarDataTypeIdx]+str(dateObj)+"_to_"+str(dateObj)+".csv")
 print("Done.")
 
